db5771f3c9551bfef626aa77a4f493814601c881
eferreyro <emafe7472@gmail.com>
2021-02-21 17:37:37 -0600

    first commit

---
+++ controllers/route.controller.php
@@ -0,0 +1,8 @@
+<?php 
+
+class RoutesController{
+/*RUTA PRINCIPAL*/
+    public function index(){
+        include "routes/route.php";
+    }   
+}
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php 
+require_once "controllers/route.controller.php";
+$index = new RoutesController();
+$index -> index();
+?>
