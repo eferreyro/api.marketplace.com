705982e1c03ea82a49e69294604e96f237e43bf3
eferreyro <emafe7472@gmail.com>
2021-02-21 17:51:41 -0600

    Agregando rutas a la API

--- index.php
+++ index.php
@@ -2,4 +2,3 @@
 require_once "controllers/route.controller.php";
 $index = new RoutesController();
 $index -> index();
-?>
--- routes/route.php
+++ routes/route.php
@@ -1,10 +1,25 @@
 <?php 
 
-$json = array(
-    "status" => 200,
-    "result" => "success"
-);
-echo json_encode($json);
-return;
+$routesArray = explode("/", $_SERVER['REQUEST_URI']);
+$routesArray = array_filter($routesArray);
+/*CUANDO NO SE HACE UNA PETICION A LA API*/
+if(count($routesArray)==0){
+    $json = array(
+        "status" => 404,
+        "result" => "Not found"
+    );
+    echo json_encode($json, http_response_code($json["status"]));
+    return;
+}else{
+    $json = array(
+        "status" => 200,
+        "result" => $routesArray[1]
+    );
+    echo json_encode($json, http_response_code($json["status"]));
+    return;
+}
+
+
+
 
 ?>
