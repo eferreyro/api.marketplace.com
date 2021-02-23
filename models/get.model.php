fca77f1d6da67c6b8cc2112056589d42a926e6fb
eferreyro <emafe7472@gmail.com>
2021-02-22 21:16:49 -0600

    Terminando las relaciones sin filtrados para las peticiones GET

--- models/get.model.php
+++ models/get.model.php
@@ -20,19 +20,57 @@ class GetModel
     }
 
    /* Peticiones GET de TABLAS RELACIONADAS sin filtro */
-    static public function getRelData($rel, $type)
-    {
+    static public function getRelData($rel, $type){
         $relArray = explode(",", $rel);
         $typeArray = explode(",", $type);
-        $on1 = $relArray[0].".id_".$typeArray[0];
-        $on2 = $relArray[1] . ".id_" . $typeArray[1];
+        /* =================================================
+        RELACIONAR 2 TABLAS 
+        =================================================*/
+        if(count($relArray) == 2 && count($typeArray) == 2){
 
-        $stmt = Connection::connect()->prepare("SELECT * FROM $relArray[0] INNER JOIN $relArray[1] ON $on1 = $on2");
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_CLASS);
+            $on1 = $relArray[0] . ".id_" . $typeArray[1] . "_" . $typeArray[0];
+            $on2 = $relArray[1] . ".id_" . $typeArray[1];
+
+            $stmt = Connection::connect()->prepare("SELECT * FROM $relArray[0] INNER JOIN $relArray[1] ON $on1 = $on2");
+
+        }
+        /* =================================================
+        RELACIONAR 3 TABLAS 
+        =================================================*/
+        if(count($relArray) == 3 && count($typeArray) == 3){
+            $on1a = $relArray[0] . ".id_" . $typeArray[1] . "_" . $typeArray[0];
+            $on1b = $relArray[1] . ".id_" . $typeArray[1];
+
+            $on2a = $relArray[0] . ".id_" . $typeArray[2] . "_" . $typeArray[0];
+            $on2b = $relArray[2] . ".id_" . $typeArray[2];
+
+            $stmt = Connection::connect()->prepare("SELECT * FROM $relArray[0]
+                 INNER JOIN $relArray[1] ON $on1a = $on1b 
+                 INNER JOIN $relArray[2] ON $on2a = $on2b");
         
+    }
+        /* =================================================
+        RELACIONAR 4 TABLAS 
+        =================================================*/
+        if (count($relArray) == 4 && count($typeArray) == 4) {
+            $on1a = $relArray[0] . ".id_" . $typeArray[1] . "_" . $typeArray[0];
+            $on1b = $relArray[1] . ".id_" . $typeArray[1];
+
+            $on2a = $relArray[0] . ".id_" . $typeArray[2] . "_" . $typeArray[0];
+            $on2b = $relArray[2] . ".id_" . $typeArray[2];
+
+            $on3a = $relArray[0] . ".id_" . $typeArray[3] . "_" . $typeArray[0];
+            $on3b = $relArray[3] . ".id_" . $typeArray[3];
+
+            $stmt = Connection::connect()->prepare("SELECT * FROM $relArray[0]
+                 INNER JOIN $relArray[1] ON $on1a = $on1b
+                 INNER JOIN $relArray[2] ON $on2a = $on2b
+                 INNER JOIN $relArray[3] ON $on3a = $on3b");
+        }
+    
+            $stmt->execute();
+            return $stmt->fetchAll(PDO::FETCH_CLASS);
     }
 }
 
 
-?>   
